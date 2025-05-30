606da950f3daff2c0e6ac5e6b152b99aaaa4656e
yawan-andri <yawan.andrisetyawan@gmail.com>
2025-05-30 16:06:52 +0700

    kantoor

---
+++ ASP_ICMS/Migrations/20250530012101_KantorChange250530.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_ICMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class KantorChange250530 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
